3e5c65c157d15523d1b1c147633ab4c3c0d206b0
Le-Caignec <robinlecaignec@yahoo.fr>
2026-02-13 18:36:14 +0100

    feat: implement ERC7984 token and mock for testing

---
+++ contracts/mocks/token/ERC7984Mock.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.28;
+
+import {ERC7984} from "../../token/ERC7984.sol";
+
+/// @dev Concrete implementation of ERC7984 for testing purposes.
+contract ERC7984Mock is ERC7984 {
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        string memory contractURI_,
+        address owner_
+    ) ERC7984(name_, symbol_, contractURI_, owner_) {}
+}
