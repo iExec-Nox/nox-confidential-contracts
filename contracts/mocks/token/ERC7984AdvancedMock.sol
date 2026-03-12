06be316e1f87f83a92e566d7159397e858a7a3b2
Zied <26070035+zguesmi@users.noreply.github.com>
2026-03-12 10:07:11 +0100

    test: Init advanced token tests

---
+++ contracts/mocks/token/ERC7984AdvancedMock.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.28;
+
+import {euint256} from "@iexec-nox/nox-protocol-contracts/contracts/sdk/Nox.sol";
+import {ERC7984Advanced} from "../../token/ERC7984Advanced.sol";
+
+contract ERC7984AdvancedMock is ERC7984Advanced {
+    constructor(
+        string memory name,
+        string memory symbol,
+        string memory contractURI
+    ) ERC7984Advanced(name, symbol, contractURI) {}
+
+    function mint(address to, euint256 amount) external returns (euint256) {
+        return _mint(to, amount);
+    }
+
+    function burn(address from, euint256 amount) external returns (euint256) {
+        return _burn(from, amount);
+    }
+
+    function transfer(address from, address to, euint256 amount) external returns (euint256) {
+        return _transfer(from, to, amount);
+    }
+}
