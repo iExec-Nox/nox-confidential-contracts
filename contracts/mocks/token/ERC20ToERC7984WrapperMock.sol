1ab42a6b88e8479f75fa1b99e35395f6117554ac
Zied <26070035+zguesmi@users.noreply.github.com>
2026-03-13 17:19:15 +0100

    test: Add tests for advanced wrapper

--- contracts/mocks/token/ERC20ToERC7984WrapperMock.sol
+++ contracts/mocks/token/ERC20ToERC7984WrapperMock.sol
@@ -1,10 +1,14 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.28;
 
+import {IERC1363Receiver} from "@openzeppelin/contracts/interfaces/IERC1363Receiver.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
+import {IERC20ToERC7984Wrapper} from "../../interfaces/IERC20ToERC7984Wrapper.sol";
+import {ERC7984Advanced} from "../../token/ERC7984Advanced.sol";
 import {ERC7984} from "../../token/ERC7984.sol";
 import {ERC20ToERC7984Wrapper} from "../../token/extensions/ERC20ToERC7984Wrapper.sol";
+import {ERC20ToERC7984WrapperAdvanced} from "../../token/extensions/ERC20ToERC7984WrapperAdvanced.sol";
 
 /// @dev Minimal ERC-20 with a public mint function, used for testing.
 contract ERC20Mock is ERC20 {
@@ -23,12 +27,27 @@ contract ERC20Mock is ERC20 {
     }
 }
 
-/// @dev Concrete implementation of ERC20ToERC7984Wrapper for testing.
-contract ERC20ToERC7984WrapperMock is ERC20ToERC7984Wrapper {
+interface ERC20ToERC7984WrapperTestableMock is IERC20ToERC7984Wrapper, IERC1363Receiver {}
+
+/// @dev Implementation of {ERC20ToERC7984Wrapper} for testing.
+contract ERC20ToERC7984WrapperMock is ERC20ToERC7984WrapperTestableMock, ERC20ToERC7984Wrapper {
     constructor(
         string memory name,
         string memory symbol,
         string memory contractURI,
         IERC20 underlying
     ) ERC7984(name, symbol, contractURI) ERC20ToERC7984Wrapper(underlying) {}
+}
+
+/// @dev Implementation of {ERC20ToERC7984WrapperAdvanced} for testing.
+contract ERC20ToERC7984WrapperAdvancedMock is
+    ERC20ToERC7984WrapperTestableMock,
+    ERC20ToERC7984WrapperAdvanced
+{
+    constructor(
+        string memory name,
+        string memory symbol,
+        string memory contractURI,
+        IERC20 underlying
+    ) ERC7984Advanced(name, symbol, contractURI) ERC20ToERC7984WrapperAdvanced(underlying) {}
 }
