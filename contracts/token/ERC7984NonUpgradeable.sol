b865cf47546fa96b70fdeb2b7c2320b04a09a405
Zied <26070035+zguesmi@users.noreply.github.com>
2026-03-26 17:35:37 +0100

    chore: Fix storage

--- contracts/token/ERC7984NonUpgradeable.sol
+++ contracts/token/ERC7984NonUpgradeable.sol
@@ -6,10 +6,9 @@ import {ERC7984Base, ERC7984Storage} from "./ERC7984Base.sol";
 
 // TODO change filename to ERC7984.sol
 abstract contract ERC7984 is ERC7984Base {
-    function _getERC7984Storage() internal pure override returns (ERC7984Storage storage $) {
-        assembly {
-            $.slot := 0x0000000000000000000000000000000000000000000000000000000000000000
-        }
+    ERC7984Storage private _erc7984Storage;
+    function _getERC7984Storage() internal view override returns (ERC7984Storage storage $) {
+        return _erc7984Storage;
     }
 
     constructor(string memory name_, string memory symbol_, string memory contractURI_) {
