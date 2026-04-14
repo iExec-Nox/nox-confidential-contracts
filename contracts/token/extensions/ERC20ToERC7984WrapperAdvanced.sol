79831f38f0bf3f595a21f244aca8bf8651d83523
Zied <26070035+zguesmi@users.noreply.github.com>
2026-04-14 17:30:03 +0200

    refactor: Update wrapper code

--- contracts/token/extensions/ERC20ToERC7984WrapperAdvanced.sol
+++ contracts/token/extensions/ERC20ToERC7984WrapperAdvanced.sol
@@ -2,27 +2,27 @@
 pragma solidity ^0.8.28;
 
 import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
-import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 import {euint256} from "@iexec-nox/nox-protocol-contracts/contracts/sdk/Nox.sol";
-import {ERC7984Base} from "../ERC7984Base.sol";
+import {ERC7984Base} from "../base/ERC7984Base.sol";
+import {ERC7984AdvancedPrimitives} from "../base/ERC7984AdvancedPrimitives.sol";
 import {ERC7984Advanced} from "../ERC7984Advanced.sol";
-import {ERC20ToERC7984Wrapper} from "./ERC20ToERC7984Wrapper.sol";
+import {ERC20ToERC7984WrapperBase} from "./ERC20ToERC7984WrapperBase.sol";
 
 /**
  * @dev Implementation of {ERC20ToERC7984Wrapper} using advanced Nox primitives.
  * See {ERC20ToERC7984Wrapper}.
  */
-abstract contract ERC20ToERC7984WrapperAdvanced is ERC20ToERC7984Wrapper, ERC7984Advanced {
-    constructor(IERC20 underlying) ERC20ToERC7984Wrapper(underlying) {}
+abstract contract ERC20ToERC7984WrapperAdvanced is ERC7984Advanced, ERC20ToERC7984WrapperBase {
+    constructor(IERC20 underlying) ERC20ToERC7984WrapperBase(underlying) {}
 
-    /// @inheritdoc ERC7984Advanced
+    /// @inheritdoc ERC7984AdvancedPrimitives
     function _update(
         address from,
         address to,
         euint256 amount
-    ) internal virtual override(ERC20ToERC7984Wrapper, ERC7984Advanced) returns (euint256) {
+    ) internal virtual override(ERC7984Base, ERC7984AdvancedPrimitives) returns (euint256) {
         if (from == address(0)) _checkConfidentialTotalSupply();
-        return ERC7984Advanced._update(from, to, amount);
+        return super._update(from, to, amount);
     }
 
     /// @inheritdoc ERC7984Base
@@ -30,16 +30,16 @@ abstract contract ERC20ToERC7984WrapperAdvanced is ERC20ToERC7984Wrapper, ERC798
         public
         view
         virtual
-        override(ERC20ToERC7984Wrapper, ERC7984Base)
+        override(ERC7984Base, ERC20ToERC7984WrapperBase)
         returns (uint8)
     {
-        return ERC20ToERC7984Wrapper.decimals();
+        return ERC20ToERC7984WrapperBase.decimals();
     }
 
-    /// @inheritdoc IERC165
+    /// @inheritdoc ERC7984Base
     function supportsInterface(
         bytes4 interfaceId
-    ) public view virtual override(ERC20ToERC7984Wrapper, ERC7984Base) returns (bool) {
-        return ERC20ToERC7984Wrapper.supportsInterface(interfaceId);
+    ) public view virtual override(ERC7984Base, ERC20ToERC7984WrapperBase) returns (bool) {
+        return ERC20ToERC7984WrapperBase.supportsInterface(interfaceId);
     }
 }
