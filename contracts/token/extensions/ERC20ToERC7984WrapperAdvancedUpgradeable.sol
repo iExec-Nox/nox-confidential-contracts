899d6abca6e3d9317f917469cd87819f6839d66b
Zied <26070035+zguesmi@users.noreply.github.com>
2026-04-15 17:54:45 +0200

    fix: Fix advance upgra wrapper

--- contracts/token/extensions/ERC20ToERC7984WrapperAdvancedUpgradeable.sol
+++ contracts/token/extensions/ERC20ToERC7984WrapperAdvancedUpgradeable.sol
@@ -3,14 +3,17 @@ pragma solidity ^0.8.28;
 
 import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
 import {Initializable} from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
-import {ERC20ToERC7984Wrapper} from "./ERC20ToERC7984Wrapper.sol";
+import {ERC20ToERC7984WrapperAdvanced} from "./ERC20ToERC7984WrapperAdvanced.sol";
 
 /**
  * @dev Upgradeable implementation of {ERC20ToERC7984WrapperAdvanced}.
  */
-abstract contract ERC20ToERC7984WrapperAdvancedUpgradeable is ERC20ToERC7984Wrapper, Initializable {
+abstract contract ERC20ToERC7984WrapperAdvancedUpgradeable is
+    ERC20ToERC7984WrapperAdvanced,
+    Initializable
+{
     // The constructor is required here to initialize immutable variables.
-    constructor(IERC20 underlying) ERC20ToERC7984Wrapper(underlying) {}
+    constructor(IERC20 underlying) ERC20ToERC7984WrapperAdvanced(underlying) {}
 
     // TODO check if this is required.
     function __ERC20ToERC7984WrapperAdvancedUpgradeable_init(
